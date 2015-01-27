688ac4e72e5cfe5fa22a31664157ebfd7280a247
Allie Sterling <sterlina89@gmail.com>
2015-01-27 13:17:30 -0800

    changed set application in deploy.rb back to bookbot

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
-set :application, 'waitlist'
+set :application, 'bookbot'
 set :repo_url, 'git@github.com:ahsterling/ec2-demo.git'
 set :use_sudo, false
 
